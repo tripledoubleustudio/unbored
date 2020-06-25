c04ed5a5f5c5bc2aee48fc208c1a55bdbc5cd7e3
tripledoubleu <tripledoubleu@Saras-MBP.lan>
2020-06-25 13:06:55 +0100

    Updates

---
+++ precache-manifest.8fca9ab777eba4c1f08eb20c0a693ff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993cc2a971eb53bb0fcb91e704e375ed",
+    "url": "/unbored/index.html"
+  },
+  {
+    "revision": "338b01e28a5ecf1b3cfd",
+    "url": "/unbored/static/css/main.bd1f14d2.chunk.css"
+  },
+  {
+    "revision": "bad52baf658e7fb640d5",
+    "url": "/unbored/static/js/2.18fce486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unbored/static/js/2.18fce486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "338b01e28a5ecf1b3cfd",
+    "url": "/unbored/static/js/main.1195d5c4.chunk.js"
+  },
+  {
+    "revision": "2cac2212c0c73ebcb0e5",
+    "url": "/unbored/static/js/runtime-main.95a24c94.js"
+  }
+]);
